e78cd6e2f482f351c9659d05f4300198fc10f352
nareshgarlapati <naresh.garlapati@gmail.com>
2020-01-24 13:47:58 -0500

    Update Test.js

--- react-sample/src/Test.js
+++ react-sample/src/Test.js
@@ -5,9 +5,15 @@ import './index.css';
 class Test extends Component {
     render(){
 
+        const divStyle = {
+            backgroundColor:"lightblue",
+            marginBottom:"40px",
+            fontFamily:"verdana"            
+        }
+
     //return <h1 style={{color:"red", backgroundColor:"blue",fontFamily:"verdana", fontSize:"30px;"}}>Hello World!</h1>;
-    return <div style={{backgroundColor:"lightblue",marginBottom:"40px"}}>
-                <h1>Sample Application</h1><br/>
+    return <div style={divStyle}>
+                <h1 style={{textAlign:"center"}}>Sample Application</h1><br/>
                 <center><div style={{marginBottom:"20px"}}> <input type="text"/> </div> </center>
            </div>;
     }
